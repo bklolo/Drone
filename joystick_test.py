12afaf65af95946944b707ad789dffb8854c8a15
bklolo <brandonlovatt@gmail.com>
2022-01-19 23:51:04 -0600

    cleaned repo

--- joystick_test.py
+++ joystick_test.py
@@ -13,14 +13,13 @@ def Joystick_Init():
     joysticks = pyglet.input.get_joysticks()
     # Ensure device(s) found
     assert joysticks, "No joystick device is connected"
-
+    # Select joystick
+    joystick = joysticks[0]
     count = 0
     for i in joysticks:
         print(count, i.device)
         count += 1
 
-	# Select joystick
-    joystick = joysticks[0]
     print(joystick.device)
     for x in joystick.device.get_controls():
         print(x)
@@ -34,7 +33,7 @@ def processInputs():
 
     x = joystick.x  # L/R
     y = joystick.y  # U/D
-    z = joystick.rz  # Throttle?
+    z = joystick.rz  # Throttle
     rz = joystick.z  # ?
     print('x: {}\ny: {}\nz: {}'.format(x, y, z))
     print()
@@ -46,19 +45,10 @@ def processInputs():
     print('xConv: {}\nyConv: {}\nzConv: {}'.format(xConv, yConv, zConv))
     print()
 
-    elements = [xConv, yConv, zConv, 0, 0, 0, 0, 30]	# caret used as break
+    elements = [xConv, yConv, zConv, 0, 0, 0, 0, 30]  # caret symbol used as break
     data = bytearray(elements)
     vect = list(data)
     print(vect)
-
-    # Write inputs to Remote Xbee
-    arduinoData.write(vect)
-
-    # # Read back data from Remote XBee
-    # arduinoString = arduinoData.readline()                # (causes delay)
-    # arduinoString = str(arduinoString, encoding='utf-8')
-    # print("from Remote: ", arduinoString)
-    # time.sleep(1)
 
 
 # Convert controller data to int for processing
@@ -71,23 +61,11 @@ def periodicFunc(dt):  # can control write data w/ specified periodicity
     print("triggered")
 
 
-def checkSerial(dt):  # serial test
-    n = arduinoData.in_waiting
-    if n > 0:
-        databytes = arduinoData.read(n)
-        arduinoString = str(databytes, encoding='utf-8')
-        print("n: ", n)
-        print("checkSerial: ", arduinoString)
-
-
 ################################# End define functions #################################
 
 
 # Initialize joystick and print controls
 Joystick_Init()
-
-# Connect to Xbee
-arduinoData = serial.Serial('COM5', 9600, serial.EIGHTBITS)
 
 
 @joystick.event
@@ -96,7 +74,7 @@ def on_joyaxis_motion(stick, axis, value):
     processInputs()
 
 
-pyglet.clock.schedule_interval(checkSerial, 0.1)
+# pyglet.clock.schedule_interval(checkSerial, 0.1)
 
 # Run app (must be last func)
 pyglet.app.run()
